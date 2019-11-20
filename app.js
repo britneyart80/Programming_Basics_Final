38efde37e2eb7b00367cebd2d7d293593a241d70
britneyart80 <chen.bri@husky.neu.edu>
2019-11-20 12:56:17 -0500

    Added styling, button events and html content.

---
+++ app.js
@@ -0,0 +1,45 @@
+const funFacts = [
+  "You can buy eel flavored ice cream in Japan.",
+  "It's considered rude to write in red ink in Portugal.",
+  "Bobcats are the most common wildcat in North America.",
+  "A cat's tail contains nearly 10 percent of all the bones in its body.",
+  "The calcium in our bones and the iron in our blood come from ancient explosions of giant stars."
+]
+
+const compliments = [
+  "I bet you make babies smile.",
+  "If cartoon bluebirds were real, a couple of 'em would be sitting on your shoulders singing right now.",
+  "I bet you sweat glitter!",
+  "Jokes are funnier when you tell them :')",
+  "You're more fun than a ball pit filled with candy."
+]
+
+const showerThoughts = [
+  "The brain named itself and declared itself the most important part of the body.",
+  "You are naturally gifted at something that hasn't been invented yet.",
+  "Wifi is a tap, cellular data is a water bottle.",
+  "A mosh pit is technically just a human Beyblade arena.",
+  "Astronauts are the ultimate combo of nerd and jock.",
+  "People are named Timothy, Timmy and Tim. Then there's Jimmy and Jim, but no Jimothy *cries*."
+]
+
+$(document).ready(function () {
+
+  $("#compliment-btn").on("click", function () {
+    const random = Math.floor(Math.random() * compliments.length)
+    $("#message").text(compliments[random])
+    $("#message-header").text("Here's a compliment!")
+  })
+
+  $("#shower-btn").on("click", function () {
+    const random = Math.floor(Math.random() * compliments.length)
+    $("#message").text(showerThoughts[random])
+    $("#message-header").text("Here's a shower thought!")
+  })
+
+  $("#fact-btn").on("click", function () {
+    const random = Math.floor(Math.random() * compliments.length)
+    $("#message").text(funFacts[random])
+    $("#message-header").text("Here's a fun fact!")
+  })
+})
